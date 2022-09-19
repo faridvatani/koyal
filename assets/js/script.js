a400bee77588c807605dc74d8b9c51ab6b742a0d
Farid Vatani <faridvatani@gmail.com>
2022-09-19 18:32:04 +0330

    style - Complete Header Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Copyright 2022 By Farid Vatani
  */
+
+/**
+ * toggle active class on header when clicked nav-toggle-btn
+ */
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
