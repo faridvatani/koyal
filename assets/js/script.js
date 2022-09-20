cfbd3233f7f482c4859ac0ed71b22cec061c9236
Farid Vatani <faridvatani@gmail.com>
2022-09-20 20:33:15 +0330

    style - added profile  menu clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,15 @@ const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
 navToggleBtn.addEventListener("click", function () {
   header.classList.toggle("active");
 });
+
+
+/**
+ * toggle ctx-menu when click on card-menu-btn
+ */
+ const menuBtn = document.querySelectorAll("[data-menu-btn]");
+
+ for (let i = 0; i < menuBtn.length; i++) {
+   menuBtn[i].addEventListener("click", function () {
+     this.nextElementSibling.classList.toggle("active");
+   });
+ }
